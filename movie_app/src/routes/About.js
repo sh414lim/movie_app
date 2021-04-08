b1ce0674f475f62c675ad797491db0acdfe49416
DESKTOP-1HI9LQN\sh414 <sh414lim@Gmail.com>
2021-04-08 09:21:00 +0900

    Movie_HashRouter

---
+++ movie_app/src/routes/About.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../routes/About.css';
+function About(props){
+    console.log(props) 
+    return<span>About this page: i buit it because i love moive</span>
+}
+
+export default About;
---
+++ movie_app/src/routes/Detail.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Detail extends React.Component{
+    
+    render(){
+    return<span>Hello</span>
+    }
+}
+
+export default Detail;
